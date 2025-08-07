c6bc96b9bc5e9d6c9d97c8b4442dc14678c88e61
mharkness1 <mharkness.uk@gmail.com>
2025-08-07 12:20:25 +0100

    B: Context colours set up. Context with colours, state with sidebar

--- src/context/colourContext.tsx
+++ src/context/colourContext.tsx
@@ -1,6 +1,35 @@
 import type { Colour } from "iris-colour";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export type ColourContextType = Colour[];
+export type ColourContextType = {
+    colours: Colour[];
+    saveColour: (col: Colour) => void;
+    updateColour: (col: Colour, id: string) => void;
+    removeColour: (id: string) => void;
+};
 
-export const ColourContext = createContext<ColourContextType>([]);
+export const ColourContext = createContext<ColourContextType | null>(null);
+
+const ColourProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [colours, setColours] = useState<Colour[]>([]);
+
+    const saveColour = (col: Colour) => {
+        setColours(prev => [...prev, col]);
+    };
+
+    const updateColour = (updated: Colour, id: string) => {
+        setColours(prev => prev.map(col => col.name === id ? updated : col))
+    };
+
+    const removeColour = (id: string) => {
+        setColours(prev => prev.filter(col => col.name !== id));
+    };
+
+    return (
+        <ColourContext.Provider value={{ colours, saveColour, updateColour, removeColour }}>
+            {children}
+        </ColourContext.Provider>
+    );
+};
+
+export { ColourProvider };
